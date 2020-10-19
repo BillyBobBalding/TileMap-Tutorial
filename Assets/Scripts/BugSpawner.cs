80ab62e12f566fbdf3183d9acf45ff3e04452c9f
BillyBobBalding <42662897+BillyBobBalding@users.noreply.github.com>
2020-10-19 14:08:09 -0500

    v1.0

---
+++ Assets/Scripts/BugSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugSpawner : MonoBehaviour
+{
+    [SerializeField] private Bug bugPrefab;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Bug newBug = Instantiate(bugPrefab);
+
+            newBug.transform.position = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField] private Tilemap tileMap;
+    [SerializeField] private List<TileData> tileDatas;
+    
+    private Dictionary<TileBase, TileData> dataFromTiles;
+
+    private void Awake()
+    {
+        dataFromTiles = new Dictionary<TileBase, TileData>();
+
+        foreach (var tileData in tileDatas)  //go into GrassTileData, go into StoneTileData
+        {
+            foreach (var tile in tileData.tiles)  //go into grass tile 1, go into grass tile 2
+            {
+                dataFromTiles.Add(tile, tileData);  //add (grass tile 1, GrassTileData), add (grass tile 2, GrassTileData)
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPos = tileMap.WorldToCell(worldPos);
+            TileBase clickedTile = tileMap.GetTile(gridPos);
+
+            float walkingSpeed = dataFromTiles[clickedTile].walkingSpeed;
+
+            //Debug.Log("GridPos: " + gridPos + "Tile: " + clickedTile);
+            Debug.Log("Walking Speed on " + clickedTile + "is: " + walkingSpeed);
+
+        }
+    }
+
+    public float GetTileWalkingSpeed(Vector2 worldPos)
+    {
+        Vector3Int gridPos = tileMap.WorldToCell(worldPos);
+        TileBase tile = tileMap.GetTile(gridPos);
+
+        if (tile == null)
+            return 1f;
+
+        float walkingSpeed = dataFromTiles[tile].walkingSpeed;
+
+        return walkingSpeed;
+    }
+
+
+}
